1b15e02f977e6c7b49b47b9d80a6a735d02dfbdf
Sourabh Mahuvar <sourabh.mahuvar@techextensor.com>
2026-01-22 12:20:02 +0530

    Task : completed practice of some patterns

--- Week01/Hand-On-Tasks/PatternPrinter.js
+++ Week01/Hand-On-Tasks/PatternPrinter.js
@@ -29,7 +29,7 @@
 // ****
 // ****
 // ****
-
+let n = 5;
 for (let i = 0; i < 4; i++) {
     let row = ""
     for (let j = 0; j < 4; j++) {
@@ -54,12 +54,112 @@ for (let i = 0; i < 5; i++) {
 
 /* another pattern
 
-
-
-
               *
              ***
             *****
            *******
 */
+
+/*
+ 1
+ 22
+ 333
+ 4444
+ 55555
+
+*/
+
+for (let i = 0; i <= n; i++) {
+    let row = "";
+   for (let j = 0; j < i; j++) {
+     row += String(i+ " ");
+   }
+   console.log(row);
+}
+
+
+// single number pattern 
+for (let i = 0; i <= n; i++) {
+    let value = 1;
+    let row = "";
+   for (let j = 0; j < i; j++) {
+     row += String(value + " ");
+   }
+   console.log(row);
+}
+console.log("\n\n");
+
+
+function printDiamond(n) {
+  // Upper half
+  for (let i = 1; i <= n; i++) {
+    let spaces = " ".repeat(n - i);
+    let stars = "*".repeat(2 * i - 1);
+    console.log(spaces + stars);
+  }
+
+  // Lower half
+  for (let i = n - 1; i >= 1; i--) {
+    let spaces = " ".repeat(n - i);
+    let stars = "*".repeat(2 * i - 1);
+    console.log(spaces + stars);
+  }
+}
+
+printDiamond(4);
+
+console.log("\n\n");
+
+
+// Floyd’s Triangle
+/*
+1 
+2 3 
+4 5 6 
+7 8 9 10
+*/
+
+let num = 1;
+
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += num + " ";
+    num++;
+  }
+  console.log(row);
+}
+
+console.log("\n\n");
+
+// inverted pyramid
+
+/*
+
+*********
+ *******
+  *****
+   ***
+    *
+
+*/
+
+
+for (let i = n; i >= 1; i--) {
+  let row = "";
+
+  for (let space = 1; space <= n - i; space++) {
+    row += " ";
+  }
+
+  for (let star = 1; star <= 2 * i - 1; star++) {
+    row += "*";
+  }
+
+  console.log(row);
+}
+
+
+
+
 
